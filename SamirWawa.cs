77334ab8c66efdaca644fc2fa7eb4828d0677236
SamirWawa <samir.wawa@iesdoctorbalmis.com>
2023-04-18 11:56:13 +0200

    Correccion

--- SamirWawa.cs
+++ SamirWawa.cs
@@ -2,6 +2,7 @@ public void AñadeAlPrincipio(NodoListaDoblementeEnlazada<T> nuevo)
 {
     nuevo.Siguiente = Primero;
     nuevo.Anterior = null;
+    Primero.Anterior = nuevo;
     Primero = nuevo;
     if (Longitud == 0) Ultimo = nuevo;
     Longitud++;
@@ -11,6 +12,7 @@ public void AñadeAlPrincipio(T dato)
     NodoListaDoblementeEnlazada<T> nuevo = new NodoListaDoblementeEnlazada<T>(dato);
     nuevo.Siguiente = Primero;
     nuevo.Anterior = null;
+    Primero.Anterior = nuevo;
     Primero = nuevo;
     if (Longitud == 0) Ultimo = nuevo;
     Longitud++;
@@ -23,6 +25,7 @@ public void AñadeAlFinal(NodoListaDoblementeEnlazada<T> nuevo)
     else
         Ultimo!.Siguiente = nuevo;
     nuevo.Anterior = Ultimo;
+    Ultimo.Siguiente = nuevo;
     Ultimo = nuevo;
     Longitud++;
 }
@@ -34,6 +37,7 @@ public void AñadeAlFinal(T dato)
     else
         Ultimo!.Siguiente = nuevo;
     nuevo.Anterior = Ultimo;
+    Ultimo.Siguiente = nuevo;
     Ultimo = nuevo;
     Longitud++;
 }
